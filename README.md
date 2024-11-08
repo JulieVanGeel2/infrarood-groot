83d4e1feda62325098c24ac38c81c2b162c8004e
JulieVanGeel2 <vangeelJ11@gmail.com>
2024-11-08 09:35:49 +0100

    Initiële bestanden voor MakeCode-project

---
+++ README.md
@@ -0,0 +1,24 @@
+
+> Open deze pagina op [https://julievangeel2.github.io/infrarood-groot/](https://julievangeel2.github.io/infrarood-groot/)
+
+## Gebruiken als extensie
+
+Deze repository kan worden toegevoegd als **extensie** in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* klik op **Nieuw project**
+* klik op **Extensies** onder het tandwielmenu
+* zoeken naar **https://github.com/julievangeel2/infrarood-groot** en importeren
+
+## Dit project bewerken
+
+Om deze repository te bewerken in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* klik op **Importeren** en klik vervolgens op **Importeer URL**
+* plak **https://github.com/julievangeel2/infrarood-groot** en klik op importeren
+
+#### Metadata (gebruikt voor zoeken, rendering)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,58 @@
+let IR_waarde = 0
+let vooruit = 94
+let achteruit = 95
+let rechts = 92
+let links = 93
+let stop = 6
+basic.forever(function () {
+    if (IR_waarde == vooruit) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 160)
+    }
+    if (IR_waarde == achteruit) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 160)
+    }
+    if (IR_waarde == links) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    }
+    if (IR_waarde == rechts) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    }
+    if (IR_waarde == stop) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    }
+})
+basic.forever(function () {
+    IR_waarde = IR.IR_read()
+})
